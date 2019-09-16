6d95857032f0a5f24bb9ab504bf44cec04c57235
Fahd <FahdShalhoub@gmail.com>
2019-09-16 06:47:48 +0300

    added routes and connection to the database

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const app = require('express')();
-const passport = require('passport');
-const LocalStrategy=require('passport-local');
+const express = require('express');
+const app=express();
 const PORT = process.env.port || 5000;
-const bodyParser = require('body-parser');
+const mongoose=require('mongoose');
+const expressLayouts=require('express-ejs-layouts');
 
+//Connecting To DB
+mongoose.connect('mongodb://localhost/chain_view',{useNewUrlParser: true})
+.then(()=>{
+    console.log('Connection To Database Successful');
+})
+.catch(err=>{
+    console.log(err);
+});
 
-app.use(bodyParser.urlencoded({extended:true}));
+//EJS
+app.use(expressLayouts);
+app.set('view engine','ejs');
+
+//Static Middleware
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended:false}));
+
+//Routers
+app.use('/',require('./routes/web'));    
+
 
 app.listen(PORT,function(){
     console.log(`The server is running on port: ${PORT}`);
