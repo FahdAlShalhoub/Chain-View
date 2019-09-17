37f4952d97c5eec8bdfb4afc040edcd2968b2a90
Fahd <FahdShalhoub@gmail.com>
2019-09-18 01:43:06 +0300

    added cookies session and flash middleware

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app=express();
 const PORT = process.env.port || 5000;
 const mongoose=require('mongoose');
 const expressLayouts=require('express-ejs-layouts');
+const cookies=require('cookie-parser');
+const session=require('express-session');
+const flash=require('express-flash');
 
 //Connecting To DB
 mongoose.connect('mongodb://localhost/chain_view',{useNewUrlParser: true})
@@ -20,6 +23,13 @@ app.set('view engine','ejs');
 //Static Middleware
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended:false}));
+app.use(session({
+    secret:'Howdy',
+    resave: true,
+    saveUninitialized: true,
+}));
+app.use(cookies());
+app.use(flash());
 
 //Routers
 app.use('/',require('./routes/web'));    
