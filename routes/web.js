3d0b069e78025b10435cffd34c19d0cc1005191e
Fahd <FahdShalhoub@gmail.com>
2019-09-18 01:47:23 +0300

    added auth middleware and passport middleware

--- routes/web.js
+++ routes/web.js
@@ -1,18 +1,42 @@
 const express=require('express');
 const router=express.Router();
 const auth=require('../controllers/auth/LoginController');
+const passport=require('passport');
+
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.get('/',function(req,res){
     res.render('home');
 });
 
 //Login
-router.get('/login',(req,res) => auth.showLoginForm(req,res));
-router.post('/login',(req,res) => auth.handleLogin(req,res));
-router.get('/logout',(req,res) => auth.handleLogout(req,res));
+router.get('/login',guest,(req,res) => auth.showLoginForm(req,res));
+router.post('/login',guest,(req,res,next) => auth.handleLogin(req,res,next));
+router.get('/logout',authed,(req,res) => auth.handleLogout(req,res));
 
 //Register
-router.get('/register',(req,res) => auth.showRegisterForm(req,res));
-router.post('/register',(req,res) => auth.handleRegistration(req,res));
+router.get('/register',guest,(req,res) => auth.showRegisterForm(req,res));
+router.post('/register',guest,(req,res) => auth.handleRegistration(req,res));
+
+//Dashboard
+router.get('/dashboard',authed,(req,res) => auth.showDashboard(req,res));
+
+//Middleware
+function authed(req,res,next){
+    if(!req.user){
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+function guest(req,res,next){
+    if(!req.user){
+        next();
+    } else {
+        res.redirect('/dashboard')
+    }
+}
 
 module.exports=router;
