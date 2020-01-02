8283e32a328723272738cebae30441591896263a
Future Technology Club <ftclub@ksu.edu.sa>
2020-01-02 15:58:29 +0300

    init generateReport

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app=express();
 const PORT = process.env.PORT || 5000;
-const database=process.env.database || 'mongodb://localhost/chain_view';
+const database="mongodb://Me:123456m@ds021166.mlab.com:21166/chain-view"
 const mongoose=require('mongoose');
 const expressLayouts=require('express-ejs-layouts');
 const session=require('express-session');
 const flash=require('express-flash');
 const fetch = require("node-fetch");
+const pdf = require("pdfkit");
+const fs = require("fs");
+const choki = require("chokidar");
 // creating transaction requirements
 var bitcoin = require("bitcoinjs-lib");
 var bigi    = require("bigi");
 var buffer  = require('buffer');
 //var keys    = new bitcoin.ECPair(bigi.fromHex(my_hex_private_key));
+
 
 
 //Connecting To DB
@@ -68,32 +72,30 @@ app.get('/transactions',(req,res)=>{
     // this endPoint shows the transaction creation page
     res.render('transactions');
 });
-app.post('/transactions', async(req,res)=>{
+app.post('/transactions',async(req,res)=>{
     //this function takes User's private Key and public key and sends everything to '/txs/new'
-       // let sender = '039f9582eab8ae6df1063bb2080a0de11d227f133bde7f401cb5014ce9ef6fef1e';
-       // let keys   = new bitcoin.ECPair(bigi.fromHex('26a27d21537ee24fb7f9fbd9e0b9e6afe2a63cbafe6775c34bfd9f5f16620895'));
+        // let sender = User.publicKey;
+       // let keys    = new bitcoin.ECPair(bigi.fromHex(User.privateKey));
         let recipient = req.body.address;
         let value = req.body.value;
-        console.log(value);
     var newtx = {
-        inputs: [{addresses: ['CEztKBAYNoUEEaPYbkyFeXC5v8Jz9RoZH9']}], // User.publicKey
-        outputs: [{addresses: ['C1rGdt7QEPGiwPMFhNKNhHmyoWpa5X92pn'], value: 100000}]
+        inputs: [{addresses: ['mqYjFiGBti4R9zQShqThN9qtNc9rBehZkc']}], // User.publicKey
+        outputs: [{addresses: [recipient], value: value}]
       };
 
-      let data = { method:'POST', body:JSON.stringify(newtx), headers: { 'Content-Type': 'application/json' }};
-      let result = await fetch('https://api.blockcypher.com/v1/bcy/test/txs/new', data);
-      let tx = await result.json(); console.log(tx);
+      let data ={method:'POST',body:JSON.stringify(newtx), headers: { 'Content-Type': 'application/json' }};
+      let tempTx = await fetch('https://api.blockcypher.com/v1/bcy/test/txs/new', data);
         // tempTx.pubkeys=[];
-        // tempTx.signatures = tempTx.tosign.map(function(tosign, n) {
-        //     tempTx.pubkeys.push(keys.getPublicKeyBuffer().toString("hex"));
+        // tempTx.signatures = tmptx.tosign.map(function(tosign, n) {
+        //     tmptx.pubkeys.push(keys.getPublicKeyBuffer().toString("hex"));
         //     return keys.sign(new buffer.Buffer(tosign, "hex")).toDER().toString("hex");
         //   });
         //   let readyData ={method:'POST',body:JSON.stringify(tempTx), headers: { 'Content-Type': 'application/json' }};
         //   let result = await fetch('https://api.blockcypher.com/v1/bcy/test/txs/send', readyData);
-      res.render('Pendingtx',{tx:tx.tx,tosign:tx.tosign});
+      console.log(tempTx);
+      res.send(tempTx);
 });
 app.get('/tx',(req,res)=>{
-
     return res.render('tx') 
 });
 app.post('/tx',async(req,res)=>{
@@ -103,19 +105,10 @@ app.post('/tx',async(req,res)=>{
    let apiLink = Link + req.body.tx;
    let result = await fetch(apiLink);
    let resultJson = await result.json();
+   
+
  return res.render("Txs",{tx:resultJson});
 });
-
-// for getting the mempool.
-app.get('/mempool',async(req,res)=>{
-
-    
-    let Link ="https://api.blockcypher.com/v1/btc/main/txs/";
-    let result = await fetch(Link);
-    let resultJson = await result.json();
-  return res.render("mempool",{tx:resultJson});
- });
-
 app.post('/address',async(req,res)=>{
 
    
@@ -123,19 +116,57 @@ app.post('/address',async(req,res)=>{
    let apiLink = Link + req.body.address;
    let result = await fetch(apiLink);
    let resultJson = await result.json();
- return res.render("address",{address:resultJson});
+   let file = fs.createWriteStream("addressReports/" + resultJson.address + ".pdf");
+   let watcher = choki.watch("addressReports/" + resultJson.address + ".pdf");
+   await writePDF(resultJson,file);
+
+
+setTimeout(()=>{
+    return res.download("addressReports/" + resultJson.address + ".pdf");
+},3000)
+  
 });
-// app.get for the mempool
-app.get('/address/:address',async(req,res)=>{
 
-   
-    let Link ="https://api.blockcypher.com/v1/btc/main/addrs/";
-    let apiLink = Link + req.params.address;
-    let result = await fetch(apiLink);
-    let resultJson = await result.json();
-  return res.render("address",{address:resultJson});
- });
-
+async function writePDF(resultJson,file){
+    let myDoc =  new pdf;
+    myDoc.pipe(file);
+    myDoc.image("public/img/logo.png",0,0,{fit: [100,100]});
+    myDoc.moveDown();
+    myDoc.font("Times-Bold");
+    myDoc.fontSize(35);
+    myDoc.text("Address",{align:"center"});
+    myDoc.fontSize(20);
+    myDoc.text(resultJson.address,{align:"center"});
+    myDoc.fontSize(30);
+    myDoc.moveDown();
+    myDoc.font("Times-Roman");
+    myDoc.fontSize(15);
+    myDoc.text("Number of Transactions: " + resultJson.n_tx);       
+    myDoc.moveDown();
+    myDoc.text("Number Of Unconfirmed Transactions: " + resultJson.unconfirmed_n_tx);
+    myDoc.moveDown();
+    myDoc.text("Balance: " + resultJson.balance + " satoshis");
+    myDoc.fontSize(20);
+    myDoc.moveDown();
+    myDoc.text("_____________________________________",{align:"center"});
+    myDoc.moveDown();
+    myDoc.font("Times-Bold");
+    myDoc.text("Latest Transactions");
+    myDoc.font("Times-Roman");
+    myDoc.fontSize(12);
+    myDoc.moveDown();
+    for(var i=0; i<5 ; i++){
+     myDoc.text("Transaction Hash: " + resultJson.txrefs[i].tx_hash);
+     myDoc.moveDown(0.1); 
+     myDoc.text("Block Height: " + resultJson.txrefs[i].block_height);
+     myDoc.moveDown(0.1); 
+     myDoc.text("Value: " + resultJson.txrefs[i].value);
+     myDoc.moveDown(0.1); 
+     myDoc.text("Date Of Confirmation: " + resultJson.txrefs[i].confirmed);       
+     myDoc.moveDown(); 
+    }
+    myDoc.end();
+}
 
 
 app.listen(PORT,function(){
